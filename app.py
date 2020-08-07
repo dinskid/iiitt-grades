96afe78e1355487e2f1e52df6519d168b9853102
Dinesh Vikram V <dineshvikram01@gmail.com>
2020-08-07 23:14:40 +0530

    add supplementary exam support

--- app.py
+++ app.py
@@ -30,7 +30,19 @@ grades[1][2] = open('./ece17.csv').readlines()
 grades[1][3] = open('./ece16.csv').readlines()
 grades[1][3] = open('./ece15.csv').readlines()
 
-results = {}
+supp = open('./supplementary.csv').readlines()
+
+supplementary = {}
+
+for row in supp:
+    roll_col = 0
+    sub_col = 1
+    grade_col = 2
+    fields = row.strip().split(',')
+    if fields[roll_col].strip() in supplementary.keys():
+        supplementary[fields[roll_col]].append((fields[sub_col], fields[grade_col]))
+    else:
+        supplementary[fields[roll_col]] = [(fields[sub_col], fields[grade_col])]
 
 def fetch_results(branch, year, email):
     '''
@@ -74,6 +86,14 @@ def fetch_results(branch, year, email):
             return results
 
     return results
+
+def fetch_supplementary(rollno):
+    supplementary_results = {}
+    if rollno in supplementary.keys():
+        for t in supplementary[rollno]:
+            supplementary_results[t[0]] = t[1]
+
+    return supplementary_results
 
 # Configuration
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
@@ -159,25 +179,25 @@ def callback():
         user['branch'] = 'CSE'
     else:
         user['branch'] = 'ECE'
+    front, rear = user['email'].split('@')
+    email2roll = open('./email_roll.csv').readlines()
+    for item in email2roll:
+        em, rno = item.split(',')
+        if em == user['email']:
+            user['rollno'] = rno.strip().upper()
+            break
+
+    if user['rollno'] == '':
+        # means they logged in from some other mail id
+        return (
+            '<h2>Please login with your institue mail id</h2>'
+            '<a href="/logout" class="btn btn-primary">Logout</a>'
+        )
     return redirect(url_for('index'))
 
 @app.route('/getresults')
 def getresults():
     if logged_in:
-        front, rear = user['email'].split('@')
-        email2roll = open('./email_roll.csv').readlines()
-        for item in email2roll:
-            em, rno = item.split(',')
-            if em == user['email']:
-                user['rollno'] = rno.strip().upper()
-                break
-
-        if user['rollno'] == '':
-            # means they logged in from some other mail id
-            return (
-                '<h2>Please login with your institue mail id</h2>'
-                '<a href="/logout" class="btn btn-primary">Logout</a>'
-            )
         year = int(user['rollno'][3:5])
         branch = user['rollno'][0].upper()
         results = fetch_results(branch.upper(), year, user['email'])
@@ -185,6 +205,25 @@ def getresults():
             return (
                 '<h2>Error has occurred. Please contact the class coordinator</h2>'
                 '<h4>Your results were not found</h4>'
+            )
+        return render_template('results.html', user=user, results=results)
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/getsupplementary')
+def getsupplementary():
+    if logged_in:
+        if user['rollno'] == '':
+            return (
+                '<h2>Error has occurred. Please contact the class coordinator</h2>'
+                '<h4>Your results were not found</h4>'
+            )
+        results = fetch_supplementary(user['rollno'])
+        if len(results) == 0:
+            return (
+                '<h1>No results</h1>'
+                '<p>If you think there is some problem and your results are missing, please contact the class coordinator or faculty advisor</p>'
+                '<a href="/">Click here to go to home page</a>'
             )
         return render_template('results.html', user=user, results=results)
     else:
